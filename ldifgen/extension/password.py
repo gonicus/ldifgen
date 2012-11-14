8df354e50211f8c2a0c856349f63ac4aca94292c
Fabian Hickert <hickert@gonicus.de>
2012-11-14 08:31:35 +0000

    Added password generate class

---
+++ ldifgen/extension/password.py
@@ -0,0 +1,26 @@
+import os
+import crypt
+import string
+import random
+from ldifgen.extension import IExtension
+
+
+class PasswordExtension(IExtension):
+    _cache = None
+
+    def __init__(self, generator):
+        super(PasswordExtension, self).__init__(generator)
+
+    def execute(self, entry, pwd=None):
+
+        print pwd
+        if not pwd:
+            pwd = ""
+            for i in range(10): #@UnusedVariable
+                pwd += random.choice(string.letters + string.digits)
+
+        salt = ""
+        for i in range(2): #@UnusedVariable
+            salt += random.choice(string.letters + string.digits)
+
+        return ["{crypt}%s" % (crypt.crypt(pwd, salt))]
